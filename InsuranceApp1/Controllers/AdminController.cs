f245e8891668a9752e176560aaa55408551a27df
Ketu68 <41552633+Ketu68@users.noreply.github.com>
2018-08-20 17:39:16 -0500

    First Insurance App

---
+++ InsuranceApp1/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using InsuranceApp1.Models;
+using InsuranceApp1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsuranceApp1.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (InsuranceEntities4 db = new InsuranceEntities4())
+            {
+                var signups = (from c in db.Insurances
+                               where c.Removed == null
+                               select c).ToList();
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.Id = signup.Id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVm.Age = signup.Age;
+                    signupVm.CarYear = signup.CarYear;
+                    signupVm.CarMake = signup.CarMake;
+                    signupVm.CarModel = signup.CarModel;
+                    signupVm.DUI = signup.DUI;
+                    signupVm.SpeedingTickets = signup.SpeedingTickets;
+                    signupVm.Coverage = signup.Coverage;
+                    signupVm.Quote = signup.Quote;
+
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms);
+            }
+        }
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (InsuranceEntities4 db = new InsuranceEntities4())
+            {
+                var signup = db.Insurances.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- InsuranceApp1/Controllers/HomeController.cs
+++ InsuranceApp1/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using InsuranceApp1.Models;
+using InsuranceApp1.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,23 +11,71 @@ namespace InsuranceApp1.Controllers
 {
     public class HomeController : Controller
     {
+        public int RunningTotal = 50;
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress, int age, int carYear, string carMake, string carModel, string dui, int speedingTickets, string coverage)
         {
-            ViewBag.Message = "Your application description page.";
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))  {
+                return View("~/Views/Shared/Error1.cshtml");
+            }
+            if (string.IsNullOrEmpty(emailAddress))  {
+                return View("~/Views/Shared/Error2.cshtml");
+            }
+            if (age == 0)
+            {
+                return View("~/Views/Shared/Error3.cshtml");
+            }
+            else if (age < 0) age = Math.Abs(age);
+            if (carYear == 0)
+            {
+                return View("~/Views/Shared/Error4.cshtml");
+            }
+            else if (carYear < 0) carYear = Math.Abs(carYear);
+            if (string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel))  {
+                return View("~/Views/Shared/Error5.cshtml");
+            }
+            if (string.IsNullOrEmpty(dui)) {
+                return View("~/Views/Shared/Error6.cshtml");
+            }
+            if (string.IsNullOrEmpty(coverage))  {
+                return View("~/Views/Shared/Error7.cshtml");
+            }
+            else
+            {
+                using (InsuranceEntities4 db = new InsuranceEntities4())
+                {
+                    var signup = new Insurance();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+                    signup.Age = age;
+                    if (age < 18) RunningTotal += 50;
+                    else if (age < 25) RunningTotal += 25;
+                    else if (age > 100) RunningTotal += 25;
+                    signup.CarYear = carYear;
+                    if ((carYear < 2000) || (carYear > 2015)) RunningTotal += 25;
+                    signup.CarMake = carMake.ToLower();
+                    if (carMake == "porsche") RunningTotal += 25;
+                    signup.CarModel = carModel.ToLower();
+                    if ((carMake == "porsche") && (carModel == "911 carrera" || carModel =="carrera")) RunningTotal += 25;
+                    signup.SpeedingTickets = speedingTickets;
+                    RunningTotal += (speedingTickets * 10);
+                    signup.DUI = dui.ToLower();
+                    if (dui == "yes" || dui == "y") RunningTotal += (RunningTotal / 4);
+                    signup.Coverage = coverage.ToLower();
+                    if(coverage == "full" || coverage == "full coverage") RunningTotal += (RunningTotal / 2);
+                    signup.Quote = RunningTotal;
 
-            return View();
-        }
+                    db.Insurances.Add(signup);
+                    db.SaveChanges();
+                }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+                return View("Success");
+            }
         }
     }
 }

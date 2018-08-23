98616c7997ae08cc3b02f5c74f0aa94b0b6a25b5
Ketu68 <41552633+Ketu68@users.noreply.github.com>
2018-08-23 16:00:22 -0500

    Updated with (model => Model.Quote)

--- InsuranceApp1/Controllers/AdminController.cs
+++ InsuranceApp1/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using InsuranceApp1.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
--- InsuranceApp1/ViewModels/SignupVm.cs
+++ InsuranceApp1/ViewModels/SignupVm.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using InsuranceApp1.Models;
+using InsuranceApp1.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace InsuranceApp1.ViewModels
 {
